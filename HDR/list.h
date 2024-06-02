0a3bf82f2527e9135248f3011c392f6978350961
Dmitriy Uryupin <urdmitriy@yandex.ru>
2024-06-02 20:21:17 +0300

    List

--- HDR/list.h
+++ HDR/list.h
@@ -12,9 +12,14 @@ typedef struct list__item_t {
 
 typedef enum {
     LIST_HEAD,
-    LIST_TAIL
+    LIST_TAIL,
+    LIST_THIS,
 }list__order_e;
 
 void* list__init(void* list);
 void* list__add_item(void* list_item, void* new_item, list__order_e order);
+void* list__exclude(void *list_item, list__order_e order);
+void* list__foreach(void *list, void *prev_item, list__order_e order);
+int list__is_empty(void *list);
+int list__count(void *list);
 #endif //LIST_H
--- SRC/list.c
+++ SRC/list.c
@@ -31,3 +31,48 @@ void* list__add_item(void* list_item, void* new_item, list__order_e order){
             break;
     }
 }
+
+void* list__exclude(void* list_item, list__order_e order){
+    list__item_t* item = 0;
+    if (list_item != 0 && ((list__item_t*) list_item)->next_list != ((list__item_t*) list_item)) {
+        switch (order) {
+            case LIST_HEAD:
+                item = ((list__item_t*) list_item)->next_list;
+                break;
+            case LIST_TAIL:
+                item = ((list__item_t*) list_item)->prev_list;
+                break;
+            case LIST_THIS:
+                item = ((list__item_t*) list_item);
+                break;
+        }
+        ((list__item_t*) item)->prev_list->next_list = ((list__item_t*) item)->next_list;
+        ((list__item_t*) item)->next_list->prev_list = ((list__item_t*) item)->prev_list;
+        ((list__item_t*) item)->next_list = ((list__item_t*) item)->prev_list = 0;
+    }
+    return item;
+}
+
+void* list__foreach(void *list, void *prev_item, list__order_e order) {
+    if (prev_item == 0)
+        prev_item = ((list__item_t*) list);
+    switch ((int) order) {
+        case LIST_HEAD:
+            return ((list__item_t*) prev_item)->next_list == list ? 0 : ((list__item_t*) prev_item)->next_list;
+        case LIST_TAIL:
+            return ((list__item_t*) prev_item)->prev_list == list ? 0 : ((list__item_t*) prev_item)->prev_list;
+    }
+    return 0;
+}
+
+int list__is_empty(void *list) {
+    if (((list__item_t*) list)->prev_list == (list__item_t*) list)
+        return 1;
+    return 0;
+}
+
+int list__count(void *list) {
+    int i = 0;
+    for (list__item_t* item = list; (item = item->next_list) != list; i++);
+    return i;
+}
